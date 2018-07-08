b6ccc9bc3226ca6f92b2503a74f27d61cebd353e
zhangyi34 <zhangyi34@meituan.com>
2018-07-09 01:51:12 +0800

    输出模块

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,8 +1,15 @@
 import { flat } from '../utils/util'
 import rawText from '../utils/raw.txt'
+import '@/vendor/semantic-ui/semantic.css'
+import img404 from '@/assets/images/404.png'
 
 const flatedArr = flat(1, [2, 3, 4], 5, 6)
 console.log(flatedArr)
 console.log(rawText)
 document.write(flatedArr)
 document.write(rawText)
+const div = document.createElement('div')
+const image = new Image()
+image.src = img404
+div.appendChild(image)
+document.querySelector('#app').appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path')
 const cleanPlugin = require('clean-webpack-plugin')
+const htmlPlugin = require('html-webpack-plugin')
+
+const mode = 'development'
 
 function resolve (d) {
   return path.resolve(__dirname, d)
 }
 
 module.exports = {
-  mode: 'production',
+  mode,
   entry: {
     app: './src/entries/app.js',
     entry: './src/entries/entry.js'
   },
-  devtool: 'source-map',
+  devtool: mode === 'development' ? 'none' : 'none',
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  },
   output: {
     path: resolve('dist'),
     filename: '[name].bundle.js'
@@ -35,10 +43,50 @@ module.exports = {
           'postcss-loader',
           'less-loader'
         ]
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000,
+            name: 'images/[name].[hash:7].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000,
+            name: 'font/[name].[hash:7].[ext]'
+          }
+        }
       }
     ]
   },
   plugins: [
-    new cleanPlugin(['dist'])
+    new cleanPlugin(['dist']),
+    new htmlPlugin({
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+      },
+      template: './index.html',
+      chunks: ['app']
+    })
   ]
 }
